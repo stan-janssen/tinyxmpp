122c0b130c57d872b847dfce41ea6195fe3e313e
Stan Jansen <stan@finetuned.nl>
2020-11-27 17:24:01 +0100

    Added description to Python module.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
+with open('README.md') as file:
+    long_description = file.read()
+
 setup(name='tinyxmpp',
-      version='0.1.0',
+      version='0.1.1',
       description='A friendly and easy to use XMPP client for Python',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Stan Janssen',
       url='https://github.com/stan-janssen/tinyxmpp',
       packages=['tinyxmpp'],
       install_requires=['lxml', 'tinysasl'])
-
